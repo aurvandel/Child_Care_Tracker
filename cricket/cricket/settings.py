d93199d00d10f4dfb210f7eb3239813baf1cb4c7
Parker <parkergw@gmail.com>
2021-01-31 17:16:32 -0700

    hid key

--- cricket/cricket/settings.py
+++ cricket/cricket/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+#use env vars to obscure secret key
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a%2-faidmw^ox0xs)-otdrz_8m-a(tu%uv8iov88^j4^@q=cq$'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
