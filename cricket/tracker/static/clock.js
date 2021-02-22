bc63b54e64d803b5dcfb1759aa3602d31bd35701
Parker <parkergw@gmail.com>
2021-02-22 10:55:30 -0700

    base.html almost working

--- cricket/tracker/static/clock.js
+++ cricket/tracker/static/clock.js
@@ -1,8 +1,24 @@
+(function() {
+    var days = ['Sun','Mon','Tues','Wedns','Thur','Fri','Sat'];
+
+    var months = ['Jan','Feb','Mar','Arp','May','June','July','Aug','Sept','Oct','Nov','Dec'];
+
+    Date.prototype.getMonthName = function() {
+        return months[ this.getMonth() ];
+    };
+    Date.prototype.getDayName = function() {
+        return days[ this.getDay() ];
+    };
+})();
+
 function currentTime() {
   var date = new Date(); /* creating object of Date class */
   var hour = date.getHours();
   var min = date.getMinutes();
   var sec = date.getSeconds();
+  var day = date.getDayName();
+  var month = date.getMonthName();
+  var year = date.getFullYear();
   var midday = "AM";
   midday = (hour >= 12) ? "PM" : "AM"; /* assigning AM/PM */
   hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12): hour); /* assigning hour in 12-hour format */
@@ -10,6 +26,7 @@ function currentTime() {
   min = updateTime(min);
   sec = updateTime(sec);
   document.getElementById("clock").innerHTML = hour + " : " + min + " : " + sec + " " + midday; /* adding time to the div */
+  document.getElementById("date").innerHTML = day + ", " + month + " " + " " + year;
   var t = setTimeout(currentTime, 1000); /* setting timer */
 }
 
