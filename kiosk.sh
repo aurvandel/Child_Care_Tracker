b1ad71c502030783aeb4f81557094dd0d160b10a
Parker <parkergw@gmail.com>
2021-03-22 07:46:58 -0600

    updated kiosk script

--- kiosk.sh
+++ kiosk.sh
@@ -11,6 +11,8 @@ unclutter -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk https://127.0.0.1:8000 &
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://127.0.0.1:8000 &
+
+python3 ~/Child_Care_Tracker/cricket/manage.py runserver
 
 
