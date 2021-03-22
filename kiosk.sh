58075d68d67a74417a0fd6c84ea5173047930bef
Parker <parkergw@gmail.com>
2021-03-22 00:26:21 -0600

    working on kiosk mode

---
+++ kiosk.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+xset s noblank	# screen won't blank
+xset s off	#screansaver disabled
+xset -dpms	# disables power management
+
+#blanks mouse faster
+unclutter -root &
+
+#removes an warnings.
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk https://127.0.0.1:8000 &
+
+
